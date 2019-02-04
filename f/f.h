c594b20b643c79717415d22e9fb7a6c2120a5a05
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2019-02-04 15:25:36 -0500

    Initial Commit

---
+++ f/f.h
@@ -0,0 +1,19 @@
+//
+//  f.h
+//  f
+//
+//  Created by Alexandre Frigon on 2019-02-04.
+//  Copyright © 2019 Frigstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for f.
+FOUNDATION_EXPORT double fVersionNumber;
+
+//! Project version string for f.
+FOUNDATION_EXPORT const unsigned char fVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <f/PublicHeader.h>
+
+
