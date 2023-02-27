944870976937a97ce1ea761fe56e5645eccb3fdc
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2023-02-27 11:28:46 -0500

    conditional view transform

--- src/UI/Extensions/View+Extension.swift
+++ src/UI/Extensions/View+Extension.swift
@@ -1,8 +1,15 @@
 import SwiftUI
 
 extension View {
-    @ViewBuilder
-    public func redacted(if condition: @autoclosure () -> Bool) -> some View {
+    @ViewBuilder public func redacted(if condition: @autoclosure () -> Bool) -> some View {
         redacted(reason: condition() ? .placeholder : [])
+    }
+
+    @ViewBuilder public func `if`<Content: View>(condition: @autoclosure () -> Bool, transform: (Self) -> Content) -> some View {
+        if condition() {
+            transform(self)
+        } else {
+            self
+        }
     }
 }
